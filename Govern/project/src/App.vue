62f7f7401709b93beafc5666726bb7582379275e
l1729 <l138829@163.com>
2025-05-12 10:28:05 +0800

    first commit

---
+++ Govern/project/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app">
+    <router-view />  <!-- 这里是关键！ -->
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+html, body {
+  background-color: #6778fe;
+  margin: 0; /* 移除默认的外边距 */
+  padding: 0; /* 移除默认的内边距 */
+}
+</style>
